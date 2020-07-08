8d8e34cfeedcc30929565142cdaa886eb094223a
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-07-08 18:26:49 -0300

    Métodos HTTP

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,42 @@ const express = require('express');
 const app = express();
 
 app.get('/projects', (request, response) => {
-    return response.json({
-        message: 'Hello World'
-    })
+    return response.json([
+        'Projeto 1',
+        'Projeto 2'
+    ])
 });
 
-app.listen(3333);
+app.post('/projects', (request, response) => {
+    return response.json(
+        [
+           'Projeto 1',
+           'Projeto 2',
+           'Projeto 3'
+        ]
+    )
+})
+
+app.put('/projects:id', (request, response) => {
+    return response.json(
+        [
+           'Projeto 5',
+           'Projeto 2',
+           'Projeto 4'
+        ]
+    )
+})
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json(
+        [
+           'Projeto 5',
+           'Projeto 2',
+        ]
+    )
+})
+
+
+app.listen(3333, () => {
+    console.log('Back-End Started!')
+});
