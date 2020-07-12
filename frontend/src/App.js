a5a1726f920e9e722534408731fcc9ec8bd44dfd
Rafael Marinho Gonçalves Ferreira <rafael.mgf2001@gmail.com>
2020-07-12 16:35:14 -0300

    Propriedades e 'children '

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,21 @@ import React from 'react';
 import Header from './components/Header';
 
 function App() {
-    return(
+    return (
         <div>
-            <Header />
-            <Header />
+            <Header title="Home Page">
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projects</li>
+                </ul>
+            </Header>
+            <Header title="Projects">
+                <ul>
+                    <li>Homepage</li>
+                    <li>Projects</li>
+                    <li>Login</li>
+                </ul>
+            </Header>
         </div>
     )
 }
